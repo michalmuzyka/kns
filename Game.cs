df4027c56d2bed16fd66b1199945eb0c00de608e
SzmurloAnia <57568819+SzmurloAnia@users.noreply.github.com>
2024-05-02 21:42:22 +0200

    Dodaj pliki projektów.

---
+++ Game.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kns;
+
+internal class Game
+{
+    const int PlayerCount = 2;
+    readonly Random random = new Random();
+    int A;
+    int N;
+    int C;
+    List<char> AvailableCharacters;
+
+
+    public Game()
+    {
+        A = ConsoleDisplay.GetParameter("a");
+        N = ConsoleDisplay.GetParameter("n");
+        C = ConsoleDisplay.GetParameter("c");
+
+        AvailableCharacters = Enumerable.Range('A', A).Select(l => (char)l).ToList();
+    }
+
+    public void Play()
+    {
+        var word = new StringBuilder();
+        int player = random.Next(PlayerCount);
+
+        while (C > 0 && word.Length < N)
+        {
+            ConsoleDisplay.DisplayPartyInfo(word.ToString(), C, N);
+            char nextLetter = GetNextChar(player);
+            word.Append(nextLetter);
+            RepetitionRemover.RemoveRepetition(word);
+
+            C--;
+
+            player = (player + 1) % PlayerCount;
+        }
+
+        ConsoleDisplay.DisplayPartyInfo(word.ToString(), C, N);
+        ConsoleDisplay.PromptWinner(word.Length == N);
+    }
+
+
+    private char GetNextChar(int player)
+    {
+        return player == 0 ? ConsoleDisplay.GetNextCharacter(AvailableCharacters) : GetEnemyChar();
+    }
+
+    private char GetEnemyChar()
+    {
+        return AvailableCharacters[random.Next(AvailableCharacters.Count)];
+    }
+
+}
---
+++ RepetitionRemover.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kns
+{
+    internal static class RepetitionRemover
+    {
+        public static void RemoveRepetition(StringBuilder word)
+        {
+            if (word == null || word.Length < 2)
+                return;
+
+            var lastPos = word.Length - 1;
+            var sameAsLastCharPostions = new List<int>();
+            for (int i=0; i < lastPos; ++i)
+            {
+                if (word[i] == word[lastPos])
+                    sameAsLastCharPostions.Add(i);
+            }
+            sameAsLastCharPostions.Reverse();
+            foreach(var pos in sameAsLastCharPostions)
+            {
+                var len = lastPos - pos;
+                if (len - 1 > pos)
+                    break;
+
+                bool ok = true;
+                for (int i=0; i<len; ++i)
+                {
+                    if (word[pos - i] != word[lastPos - i])
+                    {
+                        ok = false;
+                        break;
+                    }
+                }
+
+                if (ok)
+                {
+                    word.Remove(pos + 1, len);
+                    break;
+                }
+            }
+
+        }
+
+    }
+}
